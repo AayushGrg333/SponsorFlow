4cf86742959e15150f80f48717ac3bcd1c35e008
AayushGrg333 <aayushgrg333@gmail.com>
2025-01-19 19:52:42 +0545

    .

--- Backend/src/server.ts
+++ Backend/src/server.ts
@@ -1,4 +1,15 @@
-
+import dotenv from 'dotenv';
+dotenv.config();
 import express, {Request,Response} from 'express';
 
 const app  = express();
+const PORT = process.env.PORT || 3000;
+
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('This is the home page!');
+});
+
+app.listen(PORT,()=>{
+  console.log(`Server is runnign at ${PORT}`)
+})
