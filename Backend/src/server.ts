42d06033929009688a0f3dfeb6b58936c43952b3
AayushGrg333 <aayushgrg333@gmail.com>
2025-01-20 23:08:42 +0545

    small issue with mongodb connection resolved

--- Backend/src/server.ts
+++ Backend/src/server.ts
@@ -2,9 +2,13 @@ import dotenv from 'dotenv';
 dotenv.config();
 import express, {Request,Response} from 'express';
 
+
+import connectDB from './config/connnectdb';
+
 const app  = express();
 const PORT = process.env.PORT || 3000;
 
+connectDB();
 
 app.get('/', (req: Request, res: Response) => {
   res.send('This is the home page!');
