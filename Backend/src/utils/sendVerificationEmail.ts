0ebbdf8fafe86c8f043d4a5f7b8554b3ca7540d7
AayushGrg333 <aayushgrg333@gmail.com>
2025-02-05 21:57:23 +0545

    uninstalled npms

--- Backend/src/utils/sendVerificationEmail.ts
+++ Backend/src/utils/sendVerificationEmail.ts
@@ -1,24 +1,18 @@
 import dotenv from "dotenv";
 dotenv.config();
-import {createElement} from "react";
-import { renderToStaticMarkup } from "react-dom/server"; 
-import VerificationEmail from "../../../Shared/email/verificationEmail";
 import { Resend } from "resend";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 const sendVerificationEmail = async (username: string, email: string, verifycode: string) => {
     try {
-        // Convert React component to static HTML
-        const emailHtml = renderToStaticMarkup(
-            createElement(VerificationEmail,{username , verifycode})
-        );
+
 
         await resend.emails.send({
             from: "onboarding@resend.dev",
             to: [email],
             subject: "SponsorFlow | Verification Code",
-            html: emailHtml, 
+            html: '', 
         });
 
         return {
