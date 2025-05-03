47fa35ae426012018b2ada3057de67b8fa64ac99
AayushGrg333 <aayushgrg333@gmail.com>
2025-05-03 17:09:24 +0545

    completed signup verication testing requried

--- Backend/src/controllers/signupVerifyController.ts
+++ Backend/src/controllers/signupVerifyController.ts
@@ -4,15 +4,57 @@ import CompanyModel from "../models/Company";
 
 const verifySignupCode : RequestHandler = async (req,res) =>{
      try {
-          const username = req.params["username"];
-          const {verificationCode} = req.body;
-          res.status(200).json(
-               {
-                    success: true,
-                    message: "you are verifed",
-                    data : verificationCode
+          const { usertype , username } = req.params;
+          const { verificationCode } = req.body;
+          if(!verificationCode){
+               res.status(400).json(
+                    {
+                         success: false,
+                         message: "Verification Code is required",
+                    }
+               )
+          }
+          
+
+          const existingByUsername = usertype === "influencer"
+          ? await UserModel.findOne({ username : username}) 
+          : await CompanyModel.findOne({ username : username});
+
+          if(! existingByUsername){
+               res.status(400).json(
+                    {
+                         success: false,
+                         message: "User does not exists",
+                    }
+               ) 
+          }
+
+          if (existingByUsername.verifyCode === verificationCode){
+               if(new Date(existingByUsername.verifyCodeExpiry) > new Date){
+                    res.status(400).json(
+                         {
+                              success: false,
+                              message: "Verification code has been expired signup again to generate new code",
+                         }
+                    ) 
+               }else{
+                    res.status(200).json(
+                         {
+                              success: true,
+                              message: "User verification successful",
+                         }
+                    )
                }
-          )
+          }else{
+               res.status(400).json(
+                    {
+                         success: false,
+                         message: "Verification code is incorrect",
+                    }
+               ) 
+          }
+ 
+
      } catch (error) {
           
      }
