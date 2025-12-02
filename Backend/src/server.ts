eca5f2a59566f4b9c6a18b1e3b84dc85d3112c8c
AayushGrg333 <aayushgrg333@gmail.com>
2025-12-02 21:15:51 +0545

    cors fix #3

--- Backend/src/server.ts
+++ Backend/src/server.ts
@@ -27,15 +27,20 @@ const io = new Server(server, {
   cors: {
     origin: "http://localhost:3000",
     methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
   },
 });
 
-app.use(cors({
-  origin: 'http://localhost:3000',
-  methods: ['GET','POST','PUT','DELETE'],
+const corsOptions = {
+  origin: "http://localhost:3000",
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
-}))
+};
+
+app.use(cors(corsOptions));
+app.options("*", cors(corsOptions));
 
 async function initConnections() {
   await connectDB();
